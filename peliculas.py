b284d4ca5292bb2c67f72ead53d06535ac4cf1a1
Isabel Cristina <caleroisabelcristina5.3@gmail.com>
2025-09-01 15:22:18 -0500

    Peliculas

---
+++ peliculas.py
@@ -0,0 +1,47 @@
+# # Encantada 
+# Sinopsis de la película
+# La bella princesa Giselle es transportada por un hechizo de la malvada reina 
+# Narissa desde su mágico mundo a la moderna y caótica Manhattan actual.Inmersa
+# en un entorno que desconoce,Giselle deambula por un mundo desorganizado.
+# Duración: 1h 47m
+# # Las guerreras del k-pop
+# Sinopsis de la película
+# Un supergrupo de k-pop usa sus poderes secretos para proteger a sus fans de amenazas
+# sobrenaturales y de una banda rival de chicos decididos a robar corazones y mentes.
+# Duracion : 1h 36m
+# #spiderman
+# Sinopsis de la película
+# Luego de sufrir la picadura de una araña genéticamente modificada, un estudiante
+# de secundaria tímido y torpe adquiere increíbles capacidades como arácnido.
+# Pronto comprenderá que su misión es utilizarlas para luchar contra el mal y defender a sus vecinos.
+# Duracion:2h 6m
+# #Intensa-Mente 
+# Sinopsis de la película
+# Riley acaba de nacer y en el centro de control de su pequeña mente sólo hay sitio
+# para Alegría. Poco después aparece Tristeza y, más tarde, Ira, Miedo y Asco.
+# Las cinco emociones tendrán que ayudar a la niña cuando, ya con 11 años, su familia
+# se mude desde su idílico pueblo del Medio Oeste estadounidense a la enorme e intimidante ciudad de San Francisco.
+# Tras una serie de acontecimientos, Alegría y Tristeza tendrán que trabajar juntas para salvar a Riley.
+# Duracion: 1h 35m
+# #Barbie
+# Sinopsis de la película
+# Después de ser expulsada de Barbieland por no ser una muñeca de aspecto perfecto,
+# Barbie parte hacia el mundo humano para encontrar la verdadera felicidad.
+# Duracion:1h 54m
+# #Cómo entrenar a tu dragón 3
+# Sinopsis de la película
+# El joven vikingo Hipo parece haber conseguido que dragones y humanos convivan en paz. 
+# Sin embargo, su sueño y el de los demás habitantes de la isla de Mema no es compartido por otros vikingos,
+# especialmente por los brutales cazadores de dragones.
+# Duracion: 1h 44m
+# #Hotel Transylvania
+# Sinopsis de la película
+# El nuevo y misterioso invento de Van Helsing transforma a Drac y sus amigos en humanos,
+# y a Johnny en un monstruo. Con sus nuevos cuerpos, Drac y la manada deben encontrar
+# la manera de revertirlo antes de que se se vuelva permanente.
+# Duracion: 1h 27m
+# #turning red
+# Sinopsis de la película
+# Mei Lee, una niña de 13 años un poco rara pero segura de sí misma, que se debate
+# entre seguir siendo la hija obediente que su madre quiere que sea y el caos de la adolescencia.
+# Duracion:1h 40m
